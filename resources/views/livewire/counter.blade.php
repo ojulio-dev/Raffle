1deff6c0c62e18033411996ff46e4c308015b5d6
ojulio-dev <juliocontact15@gmail.com>
2025-05-05 11:58:15 -0300

    Tailwind CSS

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,9 +1,9 @@
-<div>
+<div class="text-white">
     {{-- Be like water. --}}
 
     Counter: {{ $count }}
 
     <br>
 
-    <button wire:click="add">Increment</button>
+    <button wire:click="add" class="bg-white text-blue-300 border-2 border-yellow-400 text-4xl">Increment</button>
 </div>
