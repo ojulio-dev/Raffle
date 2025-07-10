d0f79c0a4ae2ec86f5f8ac529c7b0aec3b0be8a7
= <=>
2025-07-10 19:31:31 -0300

    Primeiro teste de componente Livewire

--- tests/Feature/Livewire/Raffle/CreateTest.php
+++ tests/Feature/Livewire/Raffle/CreateTest.php
@@ -1,7 +1,18 @@
 <?php
 
-it('example', function() {
+use App\Livewire\Raffle\Create;
+use Livewire\Livewire;
 
-    expect(true)->toBeTrue();
+it('should create a new raffle', function() {
+
+    Livewire::test(Create::class)
+        ->set('name', 'Test Raffle')
+        ->call('handle');
+
+    $this->assertDatabaseHas('raffles', [
+
+        'name' => 'Test Raffle'
+
+    ]);
 
 });
